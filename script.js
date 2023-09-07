c1ae4301e8a16be19c27a1a111f6d1d3611ed85e
juliana615 <albrecht.2004.j@gmail.com>
2023-09-07 10:11:00 -0700

    Add and modify login.php, but seems wrong ;-)

---
+++ script.js
@@ -0,0 +1,9 @@
+// Replace 'YOUR_TWITTER_API_KEY' with your actual Twitter API Key
+function twitterLogin() {
+  // console.log("test");
+  // window.location.href = 'https://api.twitter.com/oauth/authenticate?oauth_consumer_key=YOUR_TWITTER_API_KEY&oauth_token=YOUR_OAUTH_TOKEN';
+}
+
+// document.getElementById('twitter-login-button').addEventListener('click', function() {
+//   window.location.href = 'https://api.twitter.com/oauth/authenticate?oauth_consumer_key=YOUR_TWITTER_API_KEY&oauth_token=YOUR_OAUTH_TOKEN';
+// });
